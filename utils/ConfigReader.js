00dc72be08d05c3f27bacba59e035d882c11311d
Manish Kumar Sonkar <manishsonkar677@gmail.com>
2026-06-07 09:10:44 +0530

    Initial framework setup

---
+++ utils/ConfigReader.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+    baseUrl: process.env.BASE_URL,
+    apiUrl: process.env.API_URL,
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME
+};
